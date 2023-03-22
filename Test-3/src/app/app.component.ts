4bdb1d8ce7f4e121cee8d3e0875a21ef904bdfda
unknown <kajal@gmail.com>
2023-03-22 15:24:35 +0530

    add modification

--- Test-3/src/app/app.component.ts
+++ Test-3/src/app/app.component.ts
@@ -46,11 +46,13 @@ export class AppComponent implements OnInit {
   changeStatus(data: DataType) {
     if (data.isActive) {
       console.log('true');
-      this.service.isDeactiveData(data.programID);
+      this.service.isDeactiveData(data.programID).subscribe(res=>{console.log(res);
+      })
       data.isActive = !data.isActive;
     } else {
       console.log('false');
-      this.service.isActiveData(data.programID);
+      this.service.isActiveData(data.programID).subscribe(res=>{console.log(res);
+      })
       data.isActive = !data.isActive;
     }
   }
